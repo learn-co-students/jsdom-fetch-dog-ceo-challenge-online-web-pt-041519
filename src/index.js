8724c7750597ed1c8e9b256c4b21626c6d4d0fdc
github username <github email address>
2019-11-14 22:51:05 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = 'https://dog.ceo/api/breeds/image/random/4'
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch(imgUrl)
+        .then(resp => resp.json())
+        .then(json => renderImg(json))
+
+    
+})
