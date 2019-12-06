c704b88983c1cac3b61c4de177ba7e64dbb0f712
Dan Sullivan <dansullivan19@gmail.com>
2019-12-05 21:23:11 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -57,13 +57,16 @@ function filterListByLetter() {
     let dropdown = document.getElementById("breed-dropdown");
     dropdown.addEventListener('change', (e) => {
         // alert(`You  selected: ${ e.target.value }`);
-        for (let dog of storedDogs) {
-            const dogList = document.getElementById('dog-breeds')
-            dogList.removeChild(dogList.childNodes[1])
+        const dogList = document.getElementById('dog-breeds')
+        while( dogList.hasChildNodes() ){
+            dogList.removeChild(dogList.lastChild);
         }
+        // dogList.childNodes.forEach( (li) => li.remove() )
+        // for (let dog of storedDogs) {
+        //     dogList.removeChild(dogList.childNodes[0])
+        // }
         let filteredDogs = storedDogs.filter( dog => dog.startsWith(e.target.value))
         for (let dog of filteredDogs) {
-            const dogList = document.getElementById('dog-breeds')
             const li = document.createElement('li')
             li.innerHTML = `${dog}`
             dogList.appendChild(li)
